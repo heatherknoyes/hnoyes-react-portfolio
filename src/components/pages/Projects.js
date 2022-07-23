b623de6852eaf45e09561a64b9aa67927ca34d56
Heather Noyes <heatherknoyes@gmail.com>
2022-07-22 23:26:41 -0400

    Update to add projects

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,118 +1,154 @@
 import React from "react";
 import "../../styles/Projects.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+import barcard from "../../images/barcard.png";
+import weather from "../../images/WeatherDashboardGIF.gif";
+import cheatsheet from "../../images/Cheetsheet.png";
+import workday from "../../images/WorkDaySchedulerGIF.gif";
+import quiz from "../../images/QuizGIF.gif";
+import password from "../../images/passwordGenerator.png";
+import food from "../../images/FoodRecall.gif";
 
 function Projects() {
   return (
-    <section className="portfolio">
-      <h2>Hi</h2>
-      {/* <h2 id="nav-portfolio" class="my-3">
-        Portfolio Projects
-      </h2>
-      <div class="row"></div>
-      <a
-        class="card first-project mt-4"
-        href="https://github.com/heatherknoyes/BarCard"
-      >
-        <img
-          class="img-fluid"
-          src="./assets/images/barcard.png"
-          alt="Second Project: BarCard - A Recipe Sharing Site"
-        />
-        <div class="custom-card-footer p-2">
-          <h3 class="card-title">Second Project: BarCard</h3>
-          <p class="card-text">
-            Bootstrap, Javascript, Handlebars, Express, MySQL
-          </p>
-        </div>
-      </a>
-      <div class="row justify-content-center">
-        <a
-          class="card mt-4 mx-1 col-lg-3 col-md-5 col-sm"
-          href="https://github.com/heatherknoyes/recall-near-me"
-        >
-          <img
-            src="./assets/images/FoodRecall.gif"
-            class="img-fluid"
-            alt="First Project: FDA Food Recall"
-          />
-          <div class="custom-card-footer p-2">
-            <h3 class="card-title">First Project: FDA Food Recall</h3>
-            <p class="card-text">HTML/CSS, W3.CSS, Javascript</p>
-          </div>
-        </a>
-        <a
-          class="card mt-4 mx-1 col-lg-3 col-md-5 col-sm"
-          href="https://github.com/heatherknoyes/PasswordGenerator"
-        >
-          <img
-            class="img-fluid"
-            src="./assets/images/passwordGenerator.png"
-            alt="Password Generator: An app to generate a password based on user specifications"
-          />
-          <div class="custom-card-footer p-2">
-            <h3 class="card-title">Password Generator</h3>
-            <p class="card-text">Javascript</p>
-          </div>
-        </a>
-        <a
-          class="card mt-4 mx-1 col-lg-3 col-md-5 col-sm"
-          href="https://github.com/heatherknoyes/CodingQuestionsQuiz"
-        >
-          <img
-            class="img-fluid"
-            src="./assets/images/QuizGIF.gif"
-            alt="Small video of a Javascript quiz app"
-          />
-          <div class="custom-card-footer p-2">
-            <h3 class="card-title">Javascript Quiz</h3>
-            <p class="card-text">HTML/CSS/Javascript</p>
-          </div>
-        </a>
-        <a
-          class="card mt-4 mx-1 col-lg-3 col-md-5 col-sm"
-          href="https://github.com/heatherknoyes/WorkDayScheduler"
-        >
-          <img
-            src="./assets/images/WorkDaySchedulerGIF.gif"
-            class="img-fluid"
-            alt="GIF of a workday scheduling app"
-          />
-          <div class="custom-card-footer p-2">
-            <h3 class="card-title">Workday Scheduler</h3>
-            <p class="card-text">HTML/CSS/Javascript</p>
-          </div>
-        </a>
-        <a
-          class="card mt-4 mx-1 col-lg-3 col-md-5 col-sm"
-          href="https://github.com/heatherknoyes/CSSCheatSheet"
-        >
-          <img
-            src="./assets/images/Cheetsheet.png"
-            class="img-fluid"
-            alt="First Project: CSS Cheat Sheet"
-          />
-          <div class="custom-card-footer p-2">
-            <h3 class="card-title">CSS Cheat Sheet</h3>
-            <p class="card-text">HTML/CSS</p>
-          </div>
-        </a>
-        <a
-          class="card mt-4 mx-1 col-lg-3 col-md-5 col-sm"
-          href="https://github.com/heatherknoyes/WeatherDashboard"
-        >
-          <img
-            src="./assets/images/WeatherDashboardGIF.gif"
-            class="img-fluid"
-            alt="Weather Dashboard application"
-          />
-          <div class="custom-card-footer p-2">
-            <h3 class="card-title">Weather Dashboard</h3>
-            <p class="card-text">HTML/CSS, Javascript, Bootstrap</p>
-          </div>
-        </a>
-      </div> */}
-    </section>
+    <Container className="portfolio">
+      <Row>
+        <Col sm={8}>
+          <a
+            className="card first-project mt-4"
+            href="https://github.com/heatherknoyes/BarCard"
+          >
+            <Image
+              className="img-fluid"
+              src={barcard}
+              alt="Second Project: BarCard - A Recipe Sharing Site"
+            />
+            <div className="custom-card-footer p-2">
+              <h3 className="card-title">Second Project: BarCard</h3>
+              <p className="card-text">
+                Bootstrap, Javascript, Handlebars, Express, MySQL
+              </p>
+            </div>
+          </a>
+        </Col>
+      </Row>
+      <Row className="justify-content-center">
+        <Col sm>
+          <a
+            className="card mt-4 mx-1 col-lg-3 col-md-5 col-sm"
+            href="https://github.com/heatherknoyes/recall-near-me"
+          >
+            <Image
+              src={food}
+              className="img-fluid"
+              alt="First Project: FDA Food Recall"
+            />
+            <div className="custom-card-footer p-2">
+              <h3 className="card-title">First Project: FDA Food Recall</h3>
+              <p className="card-text">HTML/CSS, W3.CSS, Javascript</p>
+            </div>
+          </a>
+        </Col>
+        <Col sm>
+          <a
+            className="card mt-4 mx-1 col-lg-3 col-md-5 col-sm"
+            href="https://github.com/heatherknoyes/PasswordGenerator"
+          >
+            <Image
+              src={password}
+              className="img-fluid"
+              alt="Password Generator: An app to generate a password based on user specifications"
+            />
+            <div className="custom-card-footer p-2">
+              <h3 className="card-title">Password Generator</h3>
+              <p className="card-text">Javascript</p>
+            </div>
+          </a>
+        </Col>
+        <Col sm>
+          <a
+            className="card mt-4 mx-1 col-lg-3 col-md-5 col-sm"
+            href="https://github.com/heatherknoyes/CodingQuestionsQuiz"
+          >
+            <Image
+              src={quiz}
+              className="img-fluid"
+              alt="Small video of a Javascript quiz app"
+            />
+            <div className="custom-card-footer p-2">
+              <h3 className="card-title">Javascript Quiz</h3>
+              <p className="card-text">HTML/CSS/Javascript</p>
+            </div>
+          </a>
+        </Col>
+        <Col sm>
+          <a
+            className="card mt-4 mx-1 col-lg-3 col-md-5 col-sm"
+            href="https://github.com/heatherknoyes/WorkDayScheduler"
+          >
+            <Image
+              src={workday}
+              className="img-fluid"
+              alt="GIF of a workday scheduling app"
+            />
+            <div className="custom-card-footer p-2">
+              <h3 className="card-title">Workday Scheduler</h3>
+              <p className="card-text">HTML/CSS/Javascript</p>
+            </div>
+          </a>
+        </Col>
+        <Col sm>
+          <a
+            className="card mt-4 mx-1 col-lg-3 col-md-5 col-sm"
+            href="https://github.com/heatherknoyes/CSSCheatSheet"
+          >
+            <Image
+              src={cheatsheet}
+              className="img-fluid"
+              alt="First Project: CSS Cheat Sheet"
+            />
+            <div className="custom-card-footer p-2">
+              <h3 className="card-title">CSS Cheat Sheet</h3>
+              <p className="card-text">HTML/CSS</p>
+            </div>
+          </a>
+        </Col>
+        <Col sm>
+          <a
+            className="card mt-4 mx-1 col-lg-3 col-md-5 col-sm"
+            href="https://github.com/heatherknoyes/WeatherDashboard"
+          >
+            <Image
+              src={weather}
+              className="img-fluid"
+              alt="Weather Dashboard application"
+            />
+            <div className="custom-card-footer p-2">
+              <h3 className="card-title">Weather Dashboard</h3>
+              <p className="card-text">HTML/CSS, Javascript, Bootstrap</p>
+            </div>
+          </a>
+        </Col>
+      </Row>
+    </Container>
   );
 }
+// return (
+//   <section className="portfolio">
+//     <h2>Hi</h2>
+{
+  /* <h2 id="nav-portfolio" class="my-3">
+        Portfolio Projects
+      </h2>
+      
+      <div class="row justify-content-center">
+         
+        
+      </div> */
+}
+// </section>
 
 export default Projects;
