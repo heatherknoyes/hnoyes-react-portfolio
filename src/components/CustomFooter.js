bab31a35aff786de45efe7121e7a0ed19cf30767
Heather Noyes <heatherknoyes@gmail.com>
2022-07-24 12:52:34 -0400

    Updated bottom margin for small screen

--- src/components/CustomFooter.js
+++ src/components/CustomFooter.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "../styles/CustomFooter.css";
-import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -8,23 +7,21 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function CustomFooter() {
   return (
     <footer>
-      <Container>
-        <Row>
-          <Col sm={{ span: 1, offset: 4 }} className="text-center">
-            <a href="https://github.com/heatherknoyes">
-              <FontAwesomeIcon icon="fa-brands fa-github" />
-            </a>
-          </Col>
-          <Col sm={2} className="text-center">
-            &copy; {new Date().getFullYear()}
-          </Col>
-          <Col sm={1} className="text-center">
-            <a href="https://www.linkedin.com/in/heatherknoyes">
-              <FontAwesomeIcon icon="fa-brands fa-linkedin" />
-            </a>
-          </Col>
-        </Row>
-      </Container>
+      <Row>
+        <Col sm={{ span: 1, offset: 4 }} className="text-center">
+          <a href="https://github.com/heatherknoyes">
+            <FontAwesomeIcon icon="fa-brands fa-github" />
+          </a>
+        </Col>
+        <Col sm={2} className="text-center">
+          &copy; {new Date().getFullYear()}
+        </Col>
+        <Col sm={1} className="text-center">
+          <a href="https://www.linkedin.com/in/heatherknoyes">
+            <FontAwesomeIcon icon="fa-brands fa-linkedin" />
+          </a>
+        </Col>
+      </Row>
     </footer>
   );
 }
